edd330e42dafb601afd85ee128b65defca3ce406
OdaNeo <odaneo@outlook.com>
2021-03-26 17:50:27 +0800

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     themeConfig: {
         nav: [
           { text: '文档', link: '/guide/' },
-          { text: '非容器化部署平台', link: 'http://1.1.1.1', target:'_blank' },
+          { text: '非容器化部署平台', link: 'http://112.126.65.241:14010/shell/installApp', target:'_blank' },
         ],
         lastUpdated: '上次更新',
         smoothScroll: true,
